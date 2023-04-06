4476e765a87a4eed32865a13d0f223eaedaf81c8
Guilherme0204 <gui2605costa@gmail.com>
2023-04-06 09:12:04 -0300

    Adicionar arquivos de projeto.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numA = 0;
+            int numB = 1;
+            int fibonacci;
+            for (int i = 0; i<20 ; i++) {
+
+
+                fibonacci = numA + numB;
+                Console.WriteLine($"Número que fazem parte da sequência de Fibonacci {fibonacci}"   );
+                numA = numB; 
+                numB = + fibonacci;
+             
+                
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+ 
